81edf89951bbaef1c19436fb221212ee6495563c
Haroon Iftikhar <haroon.iftikhar@ibuild.global>
2020-04-01 01:46:32 -0700

    Deleting EverChess

--- EverChess/EverChess/chess.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  chess.cpp
-//  EverChess
-//
-//  Created by Haroon Iftikhar on 3/26/20.
-//  Copyright © 2020 Haroon Iftikhar. All rights reserved.
-//
-
-#include <stdio.h>
--- EverChess/EverChess/user_interface.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  user_interface.cpp
-//  EverChess
-//
-//  Created by Haroon Iftikhar on 3/26/20.
-//  Copyright © 2020 Haroon Iftikhar. All rights reserved.
-//
-
-#include <stdio.h>
